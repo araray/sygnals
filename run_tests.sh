08377dd06fcc847a1949e3db86f3de45c4b28536
Araray Velho <araray@gmail.com>
2025-04-19 17:28:43 -0300

    feat

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+sygnals_test="${1:-.}"
+
 source venv/bin/activate
 
 {
-    python -m pytest . 2>&1
+    python -m pytest "${sygnals_test}" 2>&1
 } | tee -a /av/outputs/sygnals_run_$(date +'%s').txt
 
 deactivate
